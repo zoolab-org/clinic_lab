e177fe2a5db868adfd143b3697bf64adaa925476
Chun-Ying Huang <huangant@gmail.com>
2024-10-03 17:32:38 +0800

    update gw script

--- lab_net/scripts/net-gw.sh
+++ lab_net/scripts/net-gw.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo eth0: `/scripts/if.py eth0`
-	echo $0 '<gateway-ip-address>'
-	exit 1
-fi
+echo '-  eth0:' `/scripts/if.py eth0`
+echo -n '- route: '
+ip route show default
 
-GWADDR=$1
-ip route del default
-ip route add default via $GWADDR
+echo 'Press any key to continue or Ctrl-C to break ...'
+read ___
+
+if [ ! -z "$1" ]; then
+	GWADDR=$1
+	ip route del default
+	ip route add default via $GWADDR
+fi
 
 ip link add vxlan1 type vxlan id 5001 group 239.1.1.1 dev eth10 dstport 4789	# LAN
 ip link add vxlan2 type vxlan id 5002 group 239.1.1.2 dev eth20 dstport 4789	# DMZ
